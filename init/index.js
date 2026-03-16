48284af35de31cc14535cbbaae0d5d22fac6ed70
Viru2729 <viruv27official@gmail.com>
2026-03-16 11:55:20 +0530

    added the old lsitings

--- init/index.js
+++ init/index.js
@@ -4,28 +4,37 @@ const Listing = require("../models/listing.js");
 const maptilerClient = require("@maptiler/client");
 
 require("dotenv").config({ path: "../.env" });
+
+// Maptiler API setup
 maptilerClient.config.apiKey = process.env.MAP_TOKEN;
 
+// Connect to MongoDB Atlas
 async function main() {
-  await mongoose.connect("mongodb://127.0.0.1:27017/airbnb");
-  console.log("connected To DB");
+  await mongoose.connect(process.env.ATLASDB_URL);
+  console.log("Connected to MongoDB Atlas");
 
   await initDb();
 }
 
+// Initialize database
 const initDb = async () => {
+  // delete old listings
   await Listing.deleteMany({});
+  console.log("Old listings deleted");
 
   const newData = await Promise.all(
     initData.data.map(async (obj) => {
 
+      // Get coordinates from location
       const geoResponse = await maptilerClient.geocoding.forward(
         obj.location,
         { limit: 1 }
       );
 
-      obj.owner = "69a2cf9b8162135de056ba55";
+      // Set owner (your Atlas user ID)
+      obj.owner = new mongoose.Types.ObjectId("69b67d6dea5a7407de69347b");
 
+      // Add geometry
       obj.geometry = geoResponse.features[0].geometry;
 
       return obj;
@@ -34,7 +43,15 @@ const initDb = async () => {
 
   await Listing.insertMany(newData);
 
-  console.log("data Initialized with geometry");
+  console.log("Database initialized with listings + geometry");
 };
 
-main().catch((err) => console.log(err));
+// Run script
+main()
+  .then(() => {
+    console.log("Seeding completed");
+    mongoose.connection.close();
+  })
+  .catch((err) => {
+    console.log(err);
+  });
